143dffdde342305674ca1f347ba3742dc76af7c4
SkunkSonicLLC <matt@dahlbacka.net>
2025-01-16 08:41:19 -0800

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { SongProvider } from '../contexts/SongContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SongProvider>
+      <Component {...pageProps} />
+    </SongProvider>
+  );
 }
 
 export default MyApp
